55aa4c42e92566dd6bf9de9f86d71a0e62245a03
ar17092-prn335-cicloII-2019 <ar17092@ues.edu.sv>
2019-09-06 10:10:05 -0600

    Hola bebé

---
+++ src/main/java/ues/occ/edu/sv/ingenieria/prn335/cineData/entity/Factura.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ues.occ.edu.sv.ingenieria.prn335.cineData.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author root
+ */
+@Entity
+@Table(name = "factura", catalog = "cinev2", schema = "")
+@NamedQueries({
+    @NamedQuery(name = "Factura.findAll", query = "SELECT f FROM Factura f"),
+    @NamedQuery(name = "Factura.findByIdFactura", query = "SELECT f FROM Factura f WHERE f.idFactura = :idFactura"),
+    @NamedQuery(name = "Factura.findByFecha", query = "SELECT f FROM Factura f WHERE f.fecha = :fecha"),
+    @NamedQuery(name = "Factura.findByCaja", query = "SELECT f FROM Factura f WHERE f.caja = :caja")})
+public class Factura implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_factura", nullable = false)
+    private Integer idFactura;
+    @Basic(optional = false)
+    @Column(name = "fecha", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @Basic(optional = false)
+    @Column(name = "caja", nullable = false, length = 45)
+    private String caja;
+    @JoinTable(name = "pago_factura", joinColumns = {
+        @JoinColumn(name = "id_factura", referencedColumnName = "id_factura", nullable = false)}, inverseJoinColumns = {
+        @JoinColumn(name = "id_pago", referencedColumnName = "id_pago", nullable = false)})
+    @ManyToMany
+    private List<Pago> pagoList;
+    @JoinTable(name = "orden_factura", joinColumns = {
+        @JoinColumn(name = "id_factura", referencedColumnName = "id_factura", nullable = false)}, inverseJoinColumns = {
+        @JoinColumn(name = "id_orden", referencedColumnName = "id_orden", nullable = false)})
+    @ManyToMany
+    private List<Orden> ordenList;
+    @ManyToMany(mappedBy = "facturaList")
+    private List<Boleto> boletoList;
+
+    public Factura() {
+    }
+
+    public Factura(Integer idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public Factura(Integer idFactura, Date fecha, String caja) {
+        this.idFactura = idFactura;
+        this.fecha = fecha;
+        this.caja = caja;
+    }
+
+    public Integer getIdFactura() {
+        return idFactura;
+    }
+
+    public void setIdFactura(Integer idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getCaja() {
+        return caja;
+    }
+
+    public void setCaja(String caja) {
+        this.caja = caja;
+    }
+
+    public List<Pago> getPagoList() {
+        return pagoList;
+    }
+
+    public void setPagoList(List<Pago> pagoList) {
+        this.pagoList = pagoList;
+    }
+
+    public List<Orden> getOrdenList() {
+        return ordenList;
+    }
+
+    public void setOrdenList(List<Orden> ordenList) {
+        this.ordenList = ordenList;
+    }
+
+    public List<Boleto> getBoletoList() {
+        return boletoList;
+    }
+
+    public void setBoletoList(List<Boleto> boletoList) {
+        this.boletoList = boletoList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idFactura != null ? idFactura.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Factura)) {
+            return false;
+        }
+        Factura other = (Factura) object;
+        if ((this.idFactura == null && other.idFactura != null) || (this.idFactura != null && !this.idFactura.equals(other.idFactura))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ues.occ.edu.sv.ingenieria.prn335.entity.Factura[ idFactura=" + idFactura + " ]";
+    }
+    
+}
